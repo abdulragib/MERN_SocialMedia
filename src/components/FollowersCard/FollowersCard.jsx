51bcc4b15c0bd34f5e1a279d5bd1522dce321329
Abdul Ragib <abdulragib36@gmail.com>
2024-04-07 05:51:17 +0530

    PostShare done

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import './FollowersCard.css'
+import "./FollowersCard.css";
+import { Followers } from "../../Data/FollowersData";
 const FollowersCard = () => {
-  return <div className="FollowerCard">
-    <h3>Who is following you</h3>
-  </div>
+  return (
+    <div className="FollowerCard">
+      <h4>Who is following you</h4>
+      <div className="followers-container">
+      {Followers.map((follower, id) => {
+        return (
+          <div className="follower">
+            <div>
+              <img src={follower.img} alt="" className="followerImage" />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className="button fc-button">Follow</button>
+          </div>
+        );
+      })}
+    </div>
+    </div>
+  );
 };
 
 export default FollowersCard;
