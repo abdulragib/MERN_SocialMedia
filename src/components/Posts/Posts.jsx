1b045153f615e155f826006bf851fe6d2a465130
Abdul Ragib <abdulragib36@gmail.com>
2024-05-01 09:23:34 +0530

    request headers

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -11,15 +11,15 @@ const Posts = () => {
   const { user } = useSelector((state) => state.authReducer.authData);
   let { posts, loading } = useSelector((state) => state.postReducer);
   const params = useParams();
-  console.log("posts",posts)
+  console.log("posts", posts);
 
   useEffect(() => {
     dispatch(getTimeLinePosts(user._id));
   }, []);
 
-  if(!posts) return "no posts";
+  if (!posts) return "no posts";
 
-  if(params.id) posts = posts.filter(post=> post.userId === params.id);
+  if (params.id) posts = posts.filter((post) => post.userId === params.id);
 
   return (
     <div className="Posts">
