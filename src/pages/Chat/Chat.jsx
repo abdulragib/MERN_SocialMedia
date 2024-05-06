5a87bedb622f89ae1a2b7bafcbe919c2a8ef9c98
Abdul Ragib <abdulragib36@gmail.com>
2024-05-06 08:48:52 +0530

    socket io -> online user feature

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,21 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Chat.css";
 import LogoSearch from "../../components/LogoSearch/LogoSearch";
 import { useSelector } from "react-redux";
 import { userChats } from "../../api/ChatRequest";
 import Conversation from "../../components/Conversation/Conversation";
+import { Link } from "react-router-dom";
+import { UilSetting } from "@iconscout/react-unicons";
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import ChatBox from "../../components/ChatBox/ChatBox";
+import { io } from "socket.io-client";
 
 const Chat = () => {
   const { user } = useSelector((state) => state.authReducer.authData);
   console.log(user);
   const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io("http://localhost:8800");
+    socket.current.emit("new-user-add", user._id);
+    socket.current.on("get-users", (users) => {
+      setOnlineUsers(users);
+      console.log("onlineUsers", onlineUsers);
+    });
+  }, [user]);
 
   useEffect(() => {
     const getChats = async () => {
       try {
         const { data } = await userChats(user._id);
         setChats(data);
-        console.log(data);
+        console.log("chats", data);
       } catch (error) {
         console.log(error);
       }
@@ -31,8 +50,8 @@ const Chat = () => {
         <div className="Chat-container">
           <h2>Chats</h2>
           <div className="Chat-list">
-            {chats.map((chat) => (
-              <div>
+            {chats.map((chat, index) => (
+              <div key={index} onClick={() => setCurrentChat(chat)}>
                 <Conversation data={chat} currentUserId={user._id} />
               </div>
             ))}
@@ -41,7 +60,22 @@ const Chat = () => {
       </div>
 
       {/* Right Side */}
-      <div className="Right-side-chat">Right Side</div>
+      <div className="Right-side-chat">
+        <div style={{ width: "20rem", alignSelf: "flex-end" }}>
+          <div className="navIcons">
+            <Link to="../home">
+              <img src={Home} alt="" />
+            </Link>
+            <UilSetting />
+            <img src={Noti} alt="" />
+            <Link to="../chat">
+              <img src={Comment} alt="" />
+            </Link>
+          </div>
+        </div>
+        {/* chat body */}
+        <ChatBox chat={currentChat} currentUser={user._id} />
+      </div>
     </div>
   );
 };
